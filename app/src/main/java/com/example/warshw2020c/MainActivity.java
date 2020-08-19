90273820bc944aaffe97fe9a30b4a6997aa3086a
Shahar Raz <SRazstudent@gmail.com>
2020-08-19 10:53:58 +0300

    First commit, Be4 implementing health bar

--- app/src/main/java/com/example/warshw2020c/MainActivity.java
+++ app/src/main/java/com/example/warshw2020c/MainActivity.java
@@ -2,27 +2,33 @@ package com.example.warshw2020c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.icu.text.DateFormat;
 import android.os.Bundle;
 
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NUMBER_OF_ATTACKS = 3;
+    private static final int MAX_HP = 200;
+    private static final int HP_RED_BAR_VALUE = 60;
     ProgressBar pBar_main_Player1ProgressBar, pBar_main_Player2ProgressBar;
-    Button btn_main_atk1, btn_main_atk2, btn_main_atk3, btn_main_atk4, btn_main_atk5, btn_main_atk6;
-    ArrayList<Button> btnsAttack = new ArrayList<Button>();
+    ArrayList<Button> btnsPlayer_1_Attack = new ArrayList<Button>();
+    ArrayList<Button> btnsPlayer_2_Attack = new ArrayList<Button>();
 
+    TextView lbl_main_announceWinnerName;
+    ImageView img_main_winnerPic;
+    RelativeLayout lay_main_winnerAnnouncement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,110 +37,126 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(false);
 
-        associateButtons();
         setProgressBars();
-        disableOneSide(); // one side starts, the other waits
+        associateButtons();
+        chooseFirstAttacker();
+        associateWinnerTag();
 
     }
 
-    private void disableOneSide() {
-        btn_main_atk1.setEnabled(false);
-        btn_main_atk2.setEnabled(false);
-        btn_main_atk3.setEnabled(false);
+    private void associateWinnerTag() {
+        lbl_main_announceWinnerName = findViewById(R.id.lbl_main_announceWinnerName);
+        img_main_winnerPic = findViewById(R.id.img_main_winnerPic);
+        lay_main_winnerAnnouncement = findViewById(R.id.lay_main_winnerAnnouncement);
+
     }
+
+    private void chooseFirstAttacker() {
+        for(Button btn : btnsPlayer_2_Attack) btn.setEnabled(false);
+
+    }
+
 
     private void associateButtons() {
         //maybe use an array instead of seperated elements?
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk1));
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk2));
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk3));
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk4));
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk5));
-        btnsAttack.add((Button)findViewById(R.id.btn_main_atk6));
-//        btn_main_atk1 = findViewById(R.id.btn_main_atk1);
-//        btn_main_atk2 = findViewById(R.id.btn_main_atk2);
-//        btn_main_atk3 = findViewById(R.id.btn_main_atk3);
-//        btn_main_atk4 = findViewById(R.id.btn_main_atk4);
-//        btn_main_atk5 = findViewById(R.id.btn_main_atk5);
-//        btn_main_atk6 = findViewById(R.id.btn_main_atk6);
+        btnsPlayer_1_Attack.add((Button)findViewById(R.id.btn_main_atk1));
+        btnsPlayer_1_Attack.add((Button)findViewById(R.id.btn_main_atk2));
+        btnsPlayer_1_Attack.add((Button)findViewById(R.id.btn_main_atk3));
+        for(Button btn : btnsPlayer_1_Attack) btn.setOnClickListener(bottomClickeListener);
 
-        btn_main_atk1.setOnClickListener(bottomClickeListener);
-        btn_main_atk2.setOnClickListener(bottomClickeListener);
-        btn_main_atk3.setOnClickListener(bottomClickeListener);
-        btn_main_atk4.setOnClickListener(bottomClickeListener);
-        btn_main_atk5.setOnClickListener(bottomClickeListener);
-        btn_main_atk6.setOnClickListener(bottomClickeListener);
+        btnsPlayer_2_Attack.add((Button)findViewById(R.id.btn_main_atk4));
+        btnsPlayer_2_Attack.add((Button)findViewById(R.id.btn_main_atk5));
+        btnsPlayer_2_Attack.add((Button)findViewById(R.id.btn_main_atk6));
+        for(Button btn : btnsPlayer_2_Attack) btn.setOnClickListener(bottomClickeListener);
 
-
-    }
-
-    public void setProgressBars() {
-        pBar_main_Player1ProgressBar = findViewById(R.id.prgrsBar_main_Player1ProgressBar);
-        pBar_main_Player2ProgressBar =  findViewById(R.id.prgrsBar_main_Player2ProgressBar);
-        pBar_main_Player1ProgressBar.setMax(200);
-        pBar_main_Player2ProgressBar.setMax(200);
-        pBar_main_Player1ProgressBar.setProgress(200);
-        pBar_main_Player2ProgressBar.setProgress(200);
     }
 
 
     private View.OnClickListener bottomClickeListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            System.out.println("button clicked id: " + view.getId());
-            Toast.makeText(MainActivity.this, view.getTag() + "was pressed", Toast.LENGTH_SHORT).show();
+//            System.out.println("button clicked id: " + view.getId());
             bottomClicked(view);
         }
     };
 
+    public void setProgressBars() {
+        pBar_main_Player1ProgressBar = findViewById(R.id.prgrsBar_main_Player1ProgressBar);
+        pBar_main_Player2ProgressBar = findViewById(R.id.prgrsBar_main_Player2ProgressBar);
+        pBar_main_Player1ProgressBar.setBackgroundColor(getColor(R.color.lifeBarGreen));
+        pBar_main_Player2ProgressBar.setBackgroundColor(getColor(R.color.lifeBarGreen));
+        pBar_main_Player1ProgressBar.setMax(MAX_HP);
+        pBar_main_Player2ProgressBar.setMax(MAX_HP);
+        pBar_main_Player1ProgressBar.setProgress(MAX_HP);
+        pBar_main_Player2ProgressBar.setProgress(MAX_HP);
+    }
 
     private void bottomClicked(View view) {
 
         int damagedHP = reduceDamageFromOpp(view);
+        changeColor(damagedHP,view);
         toggleGroup();
         checkWinner(damagedHP, view);
 
 
     }
 
+    private void changeColor(int damagedHP,View view) {
+        if(damagedHP<HP_RED_BAR_VALUE){
+            int idOfBtn = Integer.parseInt(((Button) view).getTag().toString());
+            if (idOfBtn / NUMBER_OF_ATTACKS == 0) // player 1
+                pBar_main_Player2ProgressBar.setBackgroundColor(getColor(R.color.lifeBarRed));
+            else
+                pBar_main_Player1ProgressBar.setBackgroundColor(getColor(R.color.lifeBarRed));
+
+
+        }
+
+    }
+
     private void checkWinner(int damagedHP, View view) {
         if (damagedHP < 1) {
             int idOfBtn = Integer.parseInt(((Button) view).getTag().toString());
-            if (idOfBtn / 3 == 0)
-                announceWinner("Player1"); // change to variable String
+            if (idOfBtn / NUMBER_OF_ATTACKS == 0)
+                announceWinner(getString(R.string.Player1Name)); // change to variable String
             else
-                announceWinner("Player2"); // change to variable String
+                announceWinner(getString(R.string.Player2Name)); // change to variable String
 
-            btn_main_atk1.setEnabled(false);
-            btn_main_atk2.setEnabled(false);
-            btn_main_atk3.setEnabled(false);
-            btn_main_atk4.setEnabled(false);
-            btn_main_atk5.setEnabled(false);
-            btn_main_atk6.setEnabled(false);
+            for(Button btn : btnsPlayer_1_Attack) btn.setEnabled(false);
+            for(Button btn : btnsPlayer_2_Attack) btn.setEnabled(false);
+
         }
 
     }
 
     private void announceWinner(String playerName) {
-        Toast.makeText(MainActivity.this, playerName + " is the winner! ", Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, playerName + " is the winner! ", Toast.LENGTH_LONG).show();
+        lay_main_winnerAnnouncement.setTranslationY(0);
+        if(playerName==getString(R.string.Player2Name)){
+            img_main_winnerPic.setImageDrawable(getDrawable(R.drawable.ic_superman));
+            lbl_main_announceWinnerName.setText(getText(R.string.Player2Name) + " " + getText(R.string.winnerAnnouncement));
+        }
+        else{
+            img_main_winnerPic.setImageDrawable(getDrawable(R.drawable.ic_batman));
+            lbl_main_announceWinnerName.setText(getText(R.string.Player1Name) + " " + getText(R.string.winnerAnnouncement));
+
+        }
+        img_main_winnerPic.animate().rotation(720).alpha(100).setDuration(1000);
+        lbl_main_announceWinnerName.animate().rotation(720).alpha(100).setDuration(1000);
+
+
 
     }
 
     private void toggleGroup() {
-        if (btn_main_atk1.isEnabled()) { // TODO: 18/08/2020 use a Toggle Group
-            btn_main_atk1.setEnabled(false);
-            btn_main_atk2.setEnabled(false);
-            btn_main_atk3.setEnabled(false);
-            btn_main_atk4.setEnabled(true);
-            btn_main_atk5.setEnabled(true);
-            btn_main_atk6.setEnabled(true);
+        if (btnsPlayer_1_Attack.get(0).isEnabled()) { //
+            for(Button btn : btnsPlayer_1_Attack) btn.setEnabled(false);
+            for(Button btn : btnsPlayer_2_Attack) btn.setEnabled(true);
+
         } else {
-            btn_main_atk1.setEnabled(true);
-            btn_main_atk2.setEnabled(true);
-            btn_main_atk3.setEnabled(true);
-            btn_main_atk4.setEnabled(false);
-            btn_main_atk5.setEnabled(false);
-            btn_main_atk6.setEnabled(false);
+            for(Button btn : btnsPlayer_1_Attack) btn.setEnabled(true);
+            for(Button btn : btnsPlayer_2_Attack) btn.setEnabled(false);
+
         }
 
     }
@@ -147,24 +169,26 @@ public class MainActivity extends AppCompatActivity {
         if (idOfBtn / NUMBER_OF_ATTACKS == 0) { // player 1 is attacking
             currentHP = pBar_main_Player2ProgressBar.getProgress();
             if (idOfBtn % NUMBER_OF_ATTACKS == 0)
-                pBar_main_Player2ProgressBar.setProgress(currentHP - 10);
+                currentHP -= 10;
             else if (idOfBtn % NUMBER_OF_ATTACKS == 1)
-                pBar_main_Player2ProgressBar.setProgress(currentHP - 20);
+                currentHP -= 20;
             else //(idOfBtn%NUMBER_OF_ATTACKS==2)
-                pBar_main_Player2ProgressBar.setProgress(currentHP - 50);
+               currentHP -= 50;
 
-            return pBar_main_Player2ProgressBar.getProgress(); // updated after damage reduced
+            pBar_main_Player2ProgressBar.setProgress(currentHP);
+            return currentHP; // updated after damage reduced
 
         } else { // player 2 is attacking
             currentHP = pBar_main_Player1ProgressBar.getProgress();
             if (idOfBtn % NUMBER_OF_ATTACKS == 0)
-                pBar_main_Player1ProgressBar.setProgress(currentHP - 10);
+                currentHP -= 10;
             else if (idOfBtn % NUMBER_OF_ATTACKS == 1)
-                pBar_main_Player1ProgressBar.setProgress(currentHP - 20);
+                currentHP -= 20;
             else // (idOfBtn%NUMBER_OF_ATTACKS==2)
-                pBar_main_Player1ProgressBar.setProgress(currentHP - 50);
+                currentHP -= 50;
 
-            return pBar_main_Player1ProgressBar.getProgress(); // updated after damage reduced
+            pBar_main_Player1ProgressBar.setProgress(currentHP);
+            return currentHP; // updated after damage reduced
         }
     }
 }
